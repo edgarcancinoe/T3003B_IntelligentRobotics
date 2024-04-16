03ef3d30867ecf6c8ad64793abfd1de311d91bc2
J. Edgar Hernandez <edgarcancinoe@gmail.com>
2024-04-16 12:38:17 -0600

    Runge kutta integration method

--- Week2Challenge/puzzlebot_sim/scripts/cmd_generator.py
+++ Week2Challenge/puzzlebot_sim/scripts/cmd_generator.py
@@ -4,10 +4,9 @@ import numpy as np
 from geometry_msgs.msg import Twist
 
 def circle_motion(radius, angular_speed):
-    # Assuming radius is the radius of the circle and angular_speed is the desired angular speed
     # Calculating linear velocities for the wheels using the formula v = ω * r
-    left_velocity = angular_speed * (radius - 0.5)  # Adjusted for left wheel
-    right_velocity = angular_speed * (radius + 0.5)  # Adjusted for right wheel
+    left_velocity = angular_speed * (radius - 0.5)  
+    right_velocity = angular_speed * (radius + 0.5)
     return left_velocity, right_velocity
 
 if __name__ == '__main__':
@@ -17,7 +16,7 @@ if __name__ == '__main__':
     print('cmd generator running')
     
     radius = 1.0  # Adjust the radius of the circle as needed
-    angular_speed = 1.0  # Adjust the angular speed as needed
+    angular_speed = 0.85  # Adjust the angular speed as needed
     
     try:
         while not rospy.is_shutdown():
@@ -26,7 +25,6 @@ if __name__ == '__main__':
             twist_msg = Twist()
             twist_msg.linear.x = left_vel
             twist_msg.linear.y = right_vel
-            twist_msg.angular.z = 0.0
             
             pub.publish(twist_msg)
             rate.sleep()
--- Week2Challenge/puzzlebot_sim/scripts/puzzlebot_kinematic_model.py
+++ Week2Challenge/puzzlebot_sim/scripts/puzzlebot_kinematic_model.py
@@ -17,25 +17,8 @@ class PuzzlebotKinematicModel():
         self.r = r
         self.l = l
 
-        # Initialize PoseStamped object to keep track of pose
-        self.s: Pose = Pose()
-
-        # Initial position Point object
-        initial_position: Point = Point()
-        initial_position.x = x
-        initial_position.y = y
-        initial_position.z = 0.0 # Z is non-mutable
-
-        # Initial orientation Quaternion object (we are not using quaternion but x,y,z,1 euler rotation)
-        initial_orientation: Quaternion = Quaternion()
-        initial_orientation.x = 0.0
-        initial_orientation.y = 0.0
-        initial_orientation.z = theta # Theta
-        initial_orientation.w = 1.0
-
-        # Current state
-        self.s.position = initial_position
-        self.s.orientation = initial_orientation
+        # Initialize PoseStamped object to keep track of pose (position Z is non-mutable)
+        self.s: Pose = Pose(position = Point(x = x, y = y, z = 0.0), orientation = Quaternion(x = 0.0, y = 0.0, z = theta, w = 1.0))
 
         # Linear velocities (robot frame)
         self.v = 0.0
@@ -60,16 +43,14 @@ class PuzzlebotKinematicModel():
         self.w_r = msg.angular.y
 
     def step(self):
-
         # Compute dt based on elapsed time
         current_time = rospy.Time.now()
         dt = (current_time - self.prev_time).to_sec()
         self.prev_time = current_time
 
-
         # Option 1: Controlling v_l & v_r
         v = (self.v_r + self.v_l) / 2.0
-        theta_dot = (self.v_r - self.v_l) / self.l
+        w = (self.v_r - self.v_l) / self.l
         self.w_l = self.r * self.v_l
         self.w_r = self.r * self.v_r   
 
@@ -77,22 +58,38 @@ class PuzzlebotKinematicModel():
         # v = self.r * (self.w_r + self.w_l) / 2.0
         # theta_dot = self.r * (self.w_r - self.w_l) / self.l
 
-        x_dot = v * np.cos(self.s.orientation.z)
-        y_dot = v * np.sin(self.s.orientation.z)
+        # We capture the system's current state s = [x y theta]
+        s = np.array([self.s.position.x, self.s.position.y, self.s.orientation.z])
+        # Compute RK4 updates
+        k1 = self._state_gradient(s, v, w)
+        k2 = self._state_gradient(s + dt*k1/2.0, v, w)
+        k3 = self._state_gradient(s + dt*k2/2.0, v, w)
+        k4 = self._state_gradient(s + dt*k3, v, w)
+
+        # Update position and orientation using RK4
+        delta_pose = dt * (k1 + 2 * k2 + 2 * k3 + k4) / 6.0
+        s = s + delta_pose
         
-        # Euler numeric integration
-        self.s.position.x = self.s.position.x + x_dot * dt
-        self.s.position.y = self.s.position.y + y_dot * dt
-        self.s.orientation.z = self._wrap_to_Pi(self.s.orientation.z + theta_dot * dt)
-        
+        # Update s
+        self.s = Pose(position = Point(x = s[0], y = s[1], z = 0.0), orientation = Quaternion(x = 0.0, y = 0.0, z = s[2], w = 1.0))
+
         # Publish current state
         self.pose_publisher.publish(self._stamp(self.s))
         self.w_l_publisher.publish(self.w_l)
         self.w_r_publisher.publish(self.w_r)
+
+        # Reset velocities
         self.v_l = 0.0
         self.v_r = 0.0
         self.w_l = 0.0
         self.w_r = 0.0
+
+    def _state_gradient(self, state, v, w):
+        theta = state[2] # Get theta out of state np.array
+        x_dot = v * np.cos(theta)
+        y_dot = v * np.sin(theta)
+        theta_dot = w
+        return np.array([x_dot, y_dot, theta_dot])
 
     def _wrap_to_Pi(self, theta):
         result = np.fmod((theta + np.pi),(2 * np.pi))
@@ -102,15 +99,9 @@ class PuzzlebotKinematicModel():
         
     def _stamp(self, pose):
         # Stamp the current pose with current time
-        header = Header()
-        header.frame_id = self.frame_id
-        header.stamp = rospy.Time.now()
+        header = Header(frame_id = self.frame_id, stamp = rospy.Time.now())
         # Create the PoseStamped object with current time and pose
-        poseStamped = PoseStamped()
-        poseStamped.header = header
-        poseStamped.pose = pose
-
-        return poseStamped
+        return PoseStamped(header = header, pose = pose)
 
 freq = 100
 if __name__ == '__main__':
