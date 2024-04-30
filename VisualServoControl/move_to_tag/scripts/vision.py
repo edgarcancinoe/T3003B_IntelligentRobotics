fc41c42edb63fc5f40a3994194eccbe1acdfe2ca
J. Edgar Hernandez <edgarcancinoe@gmail.com>
2024-04-29 18:20:45 -0600

    Add camera intrinsics, vision node stores knowledge and publishes target's corners into /corners topic

--- VisualServoControl/move_to_tag/scripts/vision.py
+++ VisualServoControl/move_to_tag/scripts/vision.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import rospy
 from sensor_msgs.msg import Image
@@ -8,17 +8,23 @@ import cv2.aruco as aruco
 import numpy as np
 import tf2_ros
 import tf.transformations as tft
-from geometry_msgs.msg import TransformStamped, Vector3, Quaternion, Transform
+from geometry_msgs.msg import TransformStamped, Vector3, Quaternion, Transform, Polygon, Point
 from std_msgs.msg import Header
 
 class ArucoDetector:
-    def __init__(self, camera_topic, camera_matrix, distortion_coeffs, camera_frame_id, object_frame_id, stream_video=True):
-        rospy.init_node('aruco_detector', anonymous=True)
+    def __init__(self, camera_topic, camera_matrix, distortion_coeffs, 
+                 camera_frame_id, object_frame_id, 
+                 target_id, visual_controller_topic, 
+                 stream_video=True, verbose=True):
+        
         self.image_sub = rospy.Subscriber(camera_topic, Image, self.image_callback)
+        self.corner_pub = rospy.Publisher(visual_controller_topic, Polygon, queue_size=10)
+
         self.bridge = CvBridge()
 
         self.aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
         self.parameters = aruco.DetectorParameters_create()
+
         self.stream_video = stream_video
         self.camera_matrix = camera_matrix
         self.distortion_coeffs = distortion_coeffs
@@ -27,74 +33,108 @@ class ArucoDetector:
         self.camera_frame_id = camera_frame_id
         self.object_frame_id = object_frame_id
 
-    def image_callback(self, frame):
-        # Convert the image message to a cv image
-        cv = self.bridge.imgmsg_to_cv2(frame, "bgr8")
-        # Convert to grayscale
-        gray = cv2.cvtColor(cv, cv2.COLOR_BGR2GRAY)
+        self.target_id = target_id
         
-        # Detect the markers
-        corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, self.aruco_dict, parameters=self.parameters,
-                                                              cameraMatrix=camera_matrix, distCoeff = distortion_coeffs)
+        self.on_sight = []
+        self.knowledge = {}
+
+        self.verbose = verbose
+        
+        rospy.Timer(rospy.Duration(1.5), self._verbose)
+
+    def _verbose(self, _):
+        if self.verbose:
+            for marker in self.on_sight:
+                rospy.loginfo(f"Markers on sight: {marker} detected at {self.knowledge.get(marker)[0]}")
+            self.on_sight = []
+
+    def image_callback(self, frame):
+        # Convert the image message to cv image
+        cv = self.bridge.imgmsg_to_cv2(frame, "bgr8")
+        
+        # Detect aruco markers
+        corners, ids, rejectedImgPoints = aruco.detectMarkers(cv2.cvtColor(cv, cv2.COLOR_BGR2GRAY), self.aruco_dict, parameters=self.parameters,
+                                                              cameraMatrix=self.camera_matrix, distCoeff = self.distortion_coeffs)
+        
+        ids = ids.flatten() if ids is not None else []
 
         # Objects detected
-        if len(corners) > 0:
-            for i in range(0, len(ids)):
-                # Estimate pose of each marker
-                rvec, tvec, markerPoints = aruco.estimatePoseSingleMarkers(corners[i], 0.072, self.camera_matrix,
-                                                                        self.distortion_coeffs)
-                rvec = rvec.flatten()
-                tvec = tvec.flatten()
+        for i in range(0, len(ids)):
 
-                # Draw the marker axis and border
-                if self.stream_video:
-                    cv2.aruco.drawAxis(cv, self.camera_matrix, self.distortion_coeffs, rvec, tvec, 0.05)
-                    cv2.aruco.drawDetectedMarkers(cv, corners)
+            # Estimate pose
+            rvec, tvec, markerPoints = aruco.estimatePoseSingleMarkers(corners[i], 0.072, self.camera_matrix,
+                                                                    self.distortion_coeffs)
+            rvec = rvec.flatten()
+            tvec = tvec.flatten()
 
-                tvec[1] = -tvec[1]
-                tvec[0] = -tvec[0]
-                quat = tft.quaternion_from_euler(rvec[1], rvec[2], rvec[0])
-                
-                # Stick to the ground with custom orientation (DEBUG)
-                # tvec[1] = 0.02 - 0.09
-                # quat = tft.quaternion_from_euler(np.pi, np.pi, 0)
-                
-                rospy.loginfo(f"Marker {ids[i]} detected at {tvec}")
-                
-                # Broadcast the static transform
-                odom_transform = TransformStamped(
-                    header=Header(stamp=rospy.Time.now(), frame_id=self.camera_frame_id),
-                    child_frame_id=self.object_frame_id,
-                    transform=Transform(
-                        translation=Vector3(tvec[0], tvec[1], tvec[2]),
-                        rotation=Quaternion(quat[1], quat[2], quat[3], quat[0]) 
-                    )
+            # Draw the marker axis and border
+            if self.stream_video:
+                cv2.aruco.drawAxis(cv, self.camera_matrix, self.distortion_coeffs, rvec, tvec, 0.05)
+                cv2.aruco.drawDetectedMarkers(cv, corners)
+
+            # Adjust frame for tf
+            tvec[1] = -tvec[1]
+            tvec[0] = -tvec[0]
+
+            quat = tft.quaternion_from_euler(rvec[1], rvec[2], rvec[0])
+
+            # Stick to the ground with custom orientation (DEBUG)
+            # tvec[1] = 0.02 - 0.09
+            # quat = tft.quaternion_from_euler(np.pi, np.pi, 0)
+
+            if ids[i] not in self.on_sight:
+                self.on_sight.append(ids[i])
+                self.knowledge[ids[i]] = (tvec, quat)
+            
+            # Only send information regarding the target marker
+            if ids[i] != self.target_id:
+                continue
+
+            # Broadcast the static transform
+            self.tf_broadcaster.sendTransform(TransformStamped(
+                header=Header(stamp=rospy.Time.now(), frame_id=self.camera_frame_id),
+                child_frame_id=self.object_frame_id + str(ids[i]),
+                transform=Transform(
+                    translation=Vector3(tvec[0], tvec[1], tvec[2]),
+                    rotation=Quaternion(quat[1], quat[2], quat[3], quat[0]) 
                 )
+            ))
 
-                # Broadcast the static transform
-                self.tf_broadcaster.sendTransform(odom_transform)
+            # Broadcast corners to controller topic
+            self.corner_pub.publish(Polygon(
+                points=[Point(x=corner[0], y=corner[1]) for corner in corners[i][0]]
+            ))
 
 
-                
-                
         if self.stream_video:
             cv2.imshow('Detector', cv)
             cv2.waitKey(3)
 
 if __name__ == '__main__':
 
-    camera_matrix = np.array([[615.6994619, 0, 310.65921052],
-                          [0, 614.09468385, 221.27468646],
-                          [0, 0, 1]])
+    rospy.init_node('aruco_detector', anonymous=True)
 
-    distortion_coeffs = np.array([1.03432284e-02, 6.15742130e-02, 5.88511152e-03, -2.73265332e-04, -1.26407881])
-    
-    camera_frame_id = 'camera_link'
-    object_frame_id = 'object'
-    camera_topic = '/usb_cam/image_raw'
-    ad = ArucoDetector(camera_topic, camera_matrix, distortion_coeffs, camera_frame_id, object_frame_id, stream_video=True)
+    camera_matrix = np.array(rospy.get_param('/camera_matrix'))
+    distortion_coeffs = np.array(rospy.get_param('/distortion_coeffs'))
+    camera_frame_id = rospy.get_param('/camera_frame')
+    object_frame_id = rospy.get_param('/object_frame')
+    camera_topic = rospy.get_param('/camera_topic')
+    target_id = rospy.get_param('/target_id')
+    visual_controller_topic = rospy.get_param('/visual_controller_topic')
+
+    ad = ArucoDetector(camera_topic, 
+                       camera_matrix, 
+                       distortion_coeffs, 
+                       camera_frame_id, 
+                       object_frame_id, 
+                       target_id,
+                       visual_controller_topic, 
+                       stream_video=True)
     try:
+        rospy.logwarn('Aruco detector running')
         rospy.spin()
+
     except KeyboardInterrupt:
         print("Shutting down")
+
     cv2.destroyAllWindows()
---
+++ VisualServoControl/move_to_tag/scripts/visual_servoing.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python3
+
+import rospy
+import numpy as np
+from std_msgs.msg import Float32
+from geometry_msgs.msg import Vector3, Twist, Point, Pose, Quaternion
+from nav_msgs.msg import Odometry
+from puzzlebot_util.util import *
+
+class Puzzlebot_Visual_Controller():
+    def __init__(self, starting_pose, starting_orientation,
+                 kp_V, ki_V, kd_V, kp_w, ki_w, kd_w, 
+                 d_tolerance, deg_tolerance, commands_topic, odom_topic, control_rate):
+
+        # Proportional controller gains
+        self.kp_l = kp_V
+        self.kp_w = kp_w
+        # Integral controller gains
+        self.ki_l = ki_V
+        self.ki_w = ki_w
+        # Derivative controller gains
+        self.kd_l = kd_V
+        self.kd_w = kd_w
+
+        # Cummulative errors
+        self.ei_l = 0.0
+        self.ei_w = 0.0
+        
+        # Last error
+        self.e_l_prev = 0.0
+        self.e_w_prev = 0.0
+
+        # Reference (goal)
+        self.s_d : Point = None
+
+        # Initial state (inertial frame)
+        self.s : Pose = Pose(position=starting_pose, 
+                             orientation=Quaternion(x=0.0, y=0.0, z=starting_orientation, w=1.0))
+
+        # Set tolerances (and convert deg to radians)
+        self.w_tolerance : float = (deg_tolerance*(1/360)) * (2 * np.pi) 
+        self.d_tolerance : float = d_tolerance
+        
+        # Publishers
+        self.cmd_vel_publisher = rospy.Publisher('/' + commands_topic, Twist, queue_size=10)
+        self.error_publisher = rospy.Publisher('/errors', Twist, queue_size=10)
+        self.ref_publisher = rospy.Publisher('/ref', Twist, queue_size=10)
+        # Suscriber
+        rospy.Subscriber(odom_topic, Odometry, self.odometry_callback)
+        rospy.Timer(rospy.Duration(1.0/control_rate), self.compute_output)
+
+        # Time
+        self.time = rospy.Time.now()
+
+    def _get_dt(self):
+        current_time = rospy.Time.now()
+        dt = (current_time - self.time).to_sec()
+        self.time = current_time
+        return dt
+    
+    def _get_e_dot(self, e_l, e_w):
+        e_dot_l = e_l - self.e_l_prev
+        e_dot_w = e_w - self.e_w_prev
+        self.e_l_prev = e_l
+        self.e_w_prev = e_w
+        return e_dot_l, e_dot_w
+
+    def odometry_callback(self, msg):
+        self.s = msg.pose.pose        
+    
+    def set_goal(self, goal: Point):
+        self.ei_l = 0.0
+        self.ei_w = 0.0
+        self.e_l_prev = 0.0
+        self.e_w_prev = 0.0
+        self.s_d = goal
+        rospy.logwarn(f'Reaching for goal {self.current_goal}..')
+
+    def goal_set(self):
+        return self.s_d == None
+    
+    def _compute_errors(self):
+        # Position errors
+        e_x = self.s_d.x - self.s.position.x
+        e_y = self.s_d.y - self.s.position.y
+        # Distance error
+        e_l = np.sqrt((e_x * e_x) + (e_y * e_y))
+        
+        # Angular reference
+        w_d = np.arctan2(e_y, e_x)
+        # Angular errror
+        e_w = w_d - self.s.orientation.z
+
+        if np.abs(e_l) < self.d_tolerance:
+            e_l = 0.0
+        if np.abs(e_w) < self.w_tolerance:
+            e_w = 0.0
+
+        ref = Twist( linear = Vector3(x = self.s_d.x, y = self.s_d.y, z = 0.0),
+                    angular = Vector3(x = 0.0, y = 0.0, z = w_d))
+        
+        self.ref_publisher.publish(ref)
+        return e_l, e_w
+    
+    def compute_output(self, _):
+        # Proportional errors
+        e_l, e_w, = self._compute_errors()
+        
+        dt = self._get_dt()
+
+        # Integral error
+        self.ei_l += e_l * dt
+        self.ei_w += e_w * dt
+        
+        # Derivative error
+        e_dot_l, e_dot_w = self._get_e_dot(e_l, e_w)
+
+        errors = Twist( linear = Vector3(x = e_l, y = self.ei_l, z = e_dot_l),
+                            angular = Vector3(x = e_w, y = self.ei_w, z = e_dot_w))
+        
+        twist_msg = Twist( linear = Vector3(x = 0.0, y = 0.0, z = 0.0),
+                            angular = Vector3(x = 0.0, y = 0.0, z = 0.0))
+        
+        # Not aligned
+        if e_w != 0.0:
+            twist_msg.angular.z = (self.kp_w * e_w) + (self.ki_w * self.ei_w) + (self.kd_w * e_dot_w)
+        elif e_l != 0.0: # If aligned
+            twist_msg.linear.x = (self.kp_l * e_l) + (self.ki_l * self.ei_l) + (self.kd_l * e_dot_l)
+        else: # Reached Goal
+            rospy.logwarn(f'Goal {self.current_goal} reached')
+            self.current_goal += 1
+            self.current_goal = self.current_goal % self.n_goals
+            self.set_goal(self.goals[self.current_goal])
+
+        self.cmd_vel_publisher.publish(twist_msg)
+        self.error_publisher.publish(errors)
+
+
+
+if __name__=='__main__':
+    # Initialise and Setup node
+    rospy.init_node("visual_servoing")
+
+    # Get Global ROS parameters
+    params = get_global_params()
+
+    # Get Local ROS parameters
+    kp_V = rospy.get_param('~kp_V', 0.0)
+    ki_V = rospy.get_param('~ki_V', 0.0)
+    kd_V = rospy.get_param('~kd_V', 0.0)
+
+    kp_w = rospy.get_param('~kp_w', 0.0)
+    ki_w = rospy.get_param('~ki_w', 0.0)
+    kd_w = rospy.get_param('~kd_w', 0.0)
+
+    d_tolerance = rospy.get_param('~d_tolerance', 0.0)
+    deg_tolerance = rospy.get_param('~deg_tolerance', 0.0)
+
+    # Controller instance
+    starting_pose = Point(x = params['starting_state']['x'], 
+                          y = params['starting_state']['y'], 
+                          z = 0.0)
+    starting_orientation = params['starting_state']['theta']
+
+    desired_corner_locations = [[[308., 376.]
+                                [308., 271.]
+                                [421., 272.]
+                                [418., 380.]]]
+
+    # Initialize controller
+    # puzzlebot_controller = Puzzlebot_Visual_Controller(starting_pose = starting_pose, 
+    #                                             starting_orientation = starting_orientation, 
+    #                                             kp_V = kp_V, ki_V = ki_V, kd_V = kd_V,
+    #                                             kp_w = kp_w, ki_w = ki_w, kd_w = kd_w,
+    #                                             d_tolerance = d_tolerance, 
+    #                                             deg_tolerance = deg_tolerance, 
+    #                                             commands_topic = params['commands_topic'],
+    #                                             odom_topic = params['odometry_topic'],
+    #                                             control_rate = params['control_rate'])
+    try:
+        rospy.loginfo('The controller node is Running')
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
+
